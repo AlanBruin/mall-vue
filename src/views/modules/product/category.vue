d2ab9b131e767176f903b6e25041182e1e557402
zhengshupeng <584531053@qq.com>
2021-10-12 10:42:21 +0800

    初版提交

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+let id = 1000
+export default {
+  name: 'category',
+  data () {
+    return {
+      menus: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  mounted () {
+    this.getProduct()
+  },
+  methods: {
+    getProduct () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({data}) => {
+        console.log(data.page)
+        this.menus = data.page
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
