1a061afc65524cc744a1bff03a028bec12ed0f8c
zhengshupeng <584531053@qq.com>
2021-11-04 14:17:57 +0800

    新增批量删除

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,8 +1,36 @@
 <template>
     <div>
-        <el-tree :data="menus" :props="defaultProps" :expand-on-click-node="false" show-checkbox node-key="catId"
-                 :default-expanded-keys="expandedKey" :draggable="true" :allow-drop="allowDrop" @node-drop="handleDrop">
-            <span class="custom-tree-node" slot-scope="{ node, data }">
+        <el-switch v-model="draggable"
+                   active-text="不可拖拽"
+                   inactive-text="拖拽修改">
+        </el-switch>
+        <el-button style="margin: 0 0 0 20px"
+                   type="success"
+                   round
+                   v-if="draggable"
+                   v-on:click="updateCateGorySort">
+            批量修改
+        </el-button>
+        <el-button style="margin: 0 0 0 20px"
+                   type="danger"
+                   round
+                   v-if="!draggable"
+                   v-on:click="removeCheckedCateGory">
+            批量删除
+        </el-button>
+        <el-tree class="margin"
+                 :data="menus"
+                 :props="defaultProps"
+                 :expand-on-click-node="false"
+                 show-checkbox
+                 node-key="catId"
+                 :default-expanded-keys="expandedKey"
+                 :draggable="draggable"
+                 :allow-drop="allowDrop"
+                 @node-drop="handleDrop"
+                 ref="categoryTree">
+            <span class="custom-tree-node"
+                  slot-scope="{ node, data }">
                 <span>{{ node.label }}</span>
                 <span>
                   <el-button
@@ -35,19 +63,28 @@
             width="30%">
             <el-form :model="category">
                 <el-form-item label="分类名称">
-                    <el-input v-model="category.name" autocomplete="off"></el-input>
+                    <el-input v-model="category.name"
+                              autocomplete="off"></el-input>
                 </el-form-item>
                 <el-form-item label="分类图标">
-                    <el-input v-model="category.icon" autocomplete="off"></el-input>
+                    <el-input v-model="category.icon"
+                              autocomplete="off"></el-input>
                 </el-form-item>
                 <el-form-item label="分类单位">
-                    <el-input v-model="category.productUnit" autocomplete="off"></el-input>
+                    <el-input v-model="category.productUnit"
+                              autocomplete="off"></el-input>
                 </el-form-item>
             </el-form>
-            <span slot="footer" class="dialog-footer">
-            <el-button @click="dialogVisible = false">取 消</el-button>
-            <el-button type="primary" @click="submitCateGory">确 定</el-button>
-        </span>
+            <span slot="footer"
+                  class="dialog-footer">
+                <el-button @click="dialogVisible = false">
+                    取 消
+                </el-button>
+                <el-button type="primary"
+                           @click="submitCateGory">
+                    确 定
+                </el-button>
+            </span>
         </el-dialog>
     </div>
 </template>
@@ -64,11 +101,13 @@ const defaultCategory = {
     showStatus: 1,
     sort: 0
 };
+
 export default {
     name: "category",
     data() {
         return {
             updateNodes: [],
+            updateParentID: [],
             maxLevel: 1,
             menus: [],
             defaultProps: {
@@ -78,6 +117,7 @@ export default {
             isEdit: false,
             expandedKey: [],
             dialogVisible: false,
+            draggable: false,
             category: Object.assign({}, defaultCategory)
         };
     },
@@ -151,6 +191,23 @@ export default {
                 this.dialogVisible = false;
             });
         },
+        updateCateGorySort() {
+            this.$http({
+                url: this.$http.adornUrl("/product/category/update/sort"),
+                method: "post",
+                data: this.$http.adornData(this.updateNodes, false)
+            }).then(({ data }) => {
+                this.$message({
+                    message: "菜单修改成功",
+                    type: "success"
+                });
+                this.getProduct();
+                this.expandedKey = [this.category.parentCid];
+                this.updateNodes = [];
+                this.expandedKey = this.updateParentID;
+                this.updateParentID = [];
+            });
+        },
 
         remove(node, data) {
             this.$confirm(`是否删除【${data.name}】`, "提示", {
@@ -177,6 +234,33 @@ export default {
                     message: "已取消删除"
                 });
             });
+        },
+        removeCheckedCateGory(){
+            let checkedKeys = this.$refs.categoryTree.getCheckedKeys();
+            console.log(JSON.stringify(checkedKeys))
+            this.$confirm(`是否批量删除【${checkedKeys}】菜单`, "提示", {
+                confirmButtonText: "确定",
+                cancelButtonText: "取消",
+                type: "warning"
+            }).then(() => {
+                this.$http({
+                    url: this.$http.adornUrl("/product/category/delete"),
+                    method: "post",
+                    data: this.$http.adornData(checkedKeys, false)
+                }).then(({ data }) => {
+                    this.$message({
+                        message: "菜单批量删除成功",
+                        type: "success"
+                    });
+                    this.getProduct();
+                });
+            }).catch(() => {
+                this.$message({
+                    type: "info",
+                    message: "已取消删除"
+                });
+            });
+
         },
 
         allowDrop(draggingNode, dropNode, type) {
@@ -205,6 +289,7 @@ export default {
             } else {
                 parentNode = dropNode.parent;
             }
+            this.updateParentID.push(parentNode.data.catId);
             let newChildNodes = parentNode.childNodes;
             // 根据变更的节点，循环它的父节点下所有的子节点
             for (let i in newChildNodes) {
@@ -222,7 +307,7 @@ export default {
                             for (let j in childCateGoryNodes) {
                                 let childCategory = childCateGoryNodes[j].data;
                                 childCategory.catLevel = level + 1;
-                                let { catId, catLevel } = childCategory
+                                let { catId, catLevel } = childCategory;
                                 this.updateNodes.push({ catId, catLevel });
                             }
                         }
@@ -230,8 +315,8 @@ export default {
                 }
                 // 其他节点 只需要修改排序
                 category.sort = i;
-                let {catId , catLevel , sort ,parentCid} = category;
-                this.updateNodes.push({catId , catLevel , sort ,parentCid});
+                let { catId, catLevel, sort, parentCid } = category;
+                this.updateNodes.push({ catId, catLevel, sort, parentCid });
             }
             console.log(JSON.stringify(this.updateNodes));
         }
@@ -240,5 +325,7 @@ export default {
 </script>
 
 <style>
-
+.margin {
+    margin: 20px 0 0 0;
+}
 </style>
